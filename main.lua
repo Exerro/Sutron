533ca3b87c2e7bbe8b15bfa88b3441ee1f70fb25
wilcomega <wilcomega@gmail.com>
2013-12-10 20:49:46 +0100

    require updates and comments
    added more require statments so all the classes get loaded
    added comments to understand the code better

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+-- Define some global game variables that we will be needing later
 
 game.blockSize = 32
 game.images = { }
@@ -6,27 +7,36 @@ game.seaLevel = 128
 game.blockCountX = math.ceil( love.graphics.getWidth( ) / game.blockSize )
 game.blockCountY = math.ceil( love.graphics.getHeight( ) / game.blockSize )
 
+-- require high level classes (Core classes)
+require "Resources/Utils"
+require "Resources/Mask"
+
+-- require low level classes (Game Classes)
 require "Resources/BlockObject"
 require "Resources/InventoryObject"
 require "Resources/EntityObject"
 require "Resources/Map"
 
 function love.load( )
+	-- what will happen when the game loads
+	-- nothing....
 	
+	-- yet
 end
 
 function love.update( dt )
-	
+	-- tick
 end
 
 function love.keypressed( key, uni )
-	
+	-- keyboard press
 end
 
 function love.mousepressed( x, y, button )
-	
+	-- mouse press
 end
 
 function love.draw( )
+	-- draw these high end grapics that really impact your GTX Titan
 	love.graphics.print( game.title, 1, 1 )
 end
