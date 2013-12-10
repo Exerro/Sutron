e8b8554d33eb264a6121941745bdeda4f5401be6
awsumben13 <bluetideprograms@gmail.com>
2013-12-10 19:36:01 +0000

    Create CameraObject.lua

---
+++ Resources/CameraObject.lua
@@ -0,0 +1,27 @@
+
+game.newCameraObject = function( )
+	local t = { }
+	t.x, t.y = 1, 1
+	t.link = false
+	
+	t.move = function( self, x, y )
+		self.x, self.y = x, y
+	end
+	
+	t.link = function( self, link )
+		self.link = link
+	end
+
+	t.render = function( self, map )
+		local w, h = game.blockCountX, game.blockCountY
+		local sw, sh = love.graphics.getHeight( ), love.graphics.getWidth( )
+		for x = math.floor( self.x - w/2 ), math.ceil( self.x + w/2 ) do
+			for y = math.floor( self.y - h/2 ), math.ceil( self.y + h/2 ) do
+				if map[x] and map[x][y] then
+					map[x][y].render( ( self.x - x ) * game.blockSize + sw / 2, ( self.y - y ) * game.blockSize + sh / 2 )
+				end
+			end
+		end
+	end
+	return t
+end
